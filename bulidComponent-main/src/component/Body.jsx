7a025646139225f1b3caf80cfd1de29089e8fbbe
vicky acharjee <vickyacharjeevlg@gmail.com>
2024-10-04 01:01:13 +0530

    added for dom element manipulation, creating html from js

--- bulidComponent-main/src/component/Body.jsx
+++ bulidComponent-main/src/component/Body.jsx
@@ -4,9 +4,11 @@ import { useState,useEffect } from "react";
 import Shimmer from "./Shimmer";
 
 const Body=()=>{
-    const [listRestaurants,setListRestaurants]=useState([]);
-    const [search,setSearch]=useState('');
-    const [filterRestaurant,setFilterRestaurant]=useState([]);
+    const [listRestaurants,setListRestaurants]=useState([]); // this for restaurant list
+    const [search,setSearch]=useState(''); // this is for search
+    const [filterRestaurant,setFilterRestaurant]=useState([]); // this is for rendering the component
+    //testTing hook
+    const [test,setTest]=useState([]); // this is for the main menu
     
     useEffect(()=>{
         console.log("called");
@@ -16,39 +18,48 @@ const Body=()=>{
     const fetchData= async()=>{
         const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9127021&lng=77.5621287&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
         const json= await data.json();
-        console.log(json);
-        
+        // console.log(json);
         const res=json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants;
         setListRestaurants(res); 
         setFilterRestaurant(res);
-        console.log(res);
+        setTest(res);
     }
     if (listRestaurants.length==0) {
         return(<Shimmer/>)
     }
+    //
+
+    //
     return(
         <div className="body">
         <div>
             <button className="top-rated" onClick={()=>{
                 const filteredList=listRestaurants.filter((res)=>res.info.avgRating > 4.2);
                 // console.log(filteredList);
-                setListRestaurants(filteredList);
-        
+                setFilterRestaurant(filteredList);
             }}>
                 TOP RATED RESTAURANT
             </button>
-            
 
             <input className="filter" type="text" value={search} onChange={(e)=>setSearch(e.target.value)} />
             <button onClick={()=>{
-            //   console.log('tt',listRestaurants[0].info.name)
-               
-               
             const filterSearch=listRestaurants.filter((res)=>res.info.name.toLowerCase().includes(search.toLowerCase()));
+             if (filterSearch.length==0) {
+                 
+             }else{
+                 setFilterRestaurant(filterSearch);
+             }        
             
-                    setFilterRestaurant(filterSearch);
-                
             }} className="btn">SEARCH</button>
+            
+            {/*  */}
+            <button  onClick={()=>{
+                console.log(test)
+                setFilterRestaurant(test);
+            }}>
+                    Main Menu
+            </button>
+            {/*  */}
         </div>   
     
         <div className="res-container">
--- bulidComponent-main/src/component/Shimmer.jsx
+++ bulidComponent-main/src/component/Shimmer.jsx
@@ -2,14 +2,14 @@ const Shimmer=()=>{
     return(
         <div className="shimmer">
             <div className="shimmer-item">
-                <div className="shimmer-item-1"></div>
-                <div className="shimmer-item-1"></div>
-                <div className="shimmer-item-1"></div>
-                <div className="shimmer-item-1"></div>
-                <div className="shimmer-item-1"></div>
-                <div className="shimmer-item-1"></div>
-                <div className="shimmer-item-1"></div>
-                <div className="shimmer-item-1"></div>
+                <div className="shimmer-item-1"><div className="inner-div"></div></div>
+                <div className="shimmer-item-1"><div className="inner-div"></div></div>
+                <div className="shimmer-item-1"><div className="inner-div"></div></div>
+                <div className="shimmer-item-1"><div className="inner-div"></div></div>
+                <div className="shimmer-item-1"><div className="inner-div"></div></div>
+                <div className="shimmer-item-1"><div className="inner-div"></div></div>
+                <div className="shimmer-item-1"><div className="inner-div"></div></div>
+                <div className="shimmer-item-1"><div className="inner-div"></div></div>
             </div>
         </div>
     )
